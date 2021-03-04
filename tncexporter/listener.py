69faef69f05e20ba262c92b6e37e0278b9fbf909
rouyng <54250428+rouyng@users.noreply.github.com>
2021-03-04 16:42:57 -0700

    fix socket client code, minor cleanup and refactor

--- tncexporter/listener.py
+++ tncexporter/listener.py
@@ -6,21 +6,24 @@ from urllib.parse import urlparse
 import socket
 
 # AGWPE format packet to request version from TNC host
-version_request = bytes.fromhex(
-    "000000005200000000000000000000000000000000000000000000000000000000000000")
+VERSION_REQUEST = b"\x00\x00\x00\x00\x52\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+                  b"\x00\x00\x00\x00"
 
 # AGWPE format packet to request monitoring packets be send from host
-monitor_request = bytes.fromhex(
-    "000000006D00000000000000000000000000000000000000000000000000000000000000")
+MONITOR_REQUEST = b"\x00\x00\x00\x00\x6D\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+                  b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" \
+                  b"\x00\x00\x00\x00"
 
 
 class Listener:
     """Class for creating listener objects that connect to AGWPE TCP/IP API and capture packets"""
     def __init__(self, tnc_url: str):
         self.parsed_url = urlparse(tnc_url)
-        self.tnc_host = self.parsed_url.netloc  # tnc host to connect to
-        self.tnc_port = self.parsed_url.port  # tnc port to listen on
-        self.packets = []  # queue of packets to be parsed/processed by exporter
+        self.tnc_host = self.parsed_url.hostname  # tnc host to connect to
+        self.tnc_port = int(self.parsed_url.port)  # tnc port to listen on
+        self.packets = []  # queue of raw packet bytestrings to be parsed/processed by exporter
+        self.decoded_packets = []  # list of PacketInfo objects decoded from packet bytestrings
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect(self.tnc_host, self.tnc_port)
         self.api_version = None  # version returned by host API
@@ -30,18 +33,16 @@ class Listener:
         self.client_socket.connect((host, port))
         # TODO: send "R" packet, receive version number
         # TODO: send "g" packet, receive port capabilities
-        self.client_socket.sendall(monitor_request)  # ask tnc to send monitor packets
+        self.client_socket.sendall(MONITOR_REQUEST)  # ask tnc to send monitor packets
 
-    def receive(self):
-        chunks = []
+    def receive_packet(self):
+        """Receive a packet from the AGWPE API and append it to the packet list as a bytestring"""
+        chunks = b""
         bytes_recv = 0
-        # TODO: modify chunk receiving algorithim to detect packet lenght, which is
-        # 4 bytes encoding an unsigned integer at positions 29-33
         while bytes_recv < 36:
-            chunk = self.client_socket.recv(36)
+            chunk = self.client_socket.recv(4096)
             if chunk == b'':
                 raise Exception("Socket connection broken")
-            chunks.append(chunk)
+            chunks += chunk
             bytes_recv += len(chunk)
-        packet = chunks
-        self.packets.append(packet)
+        self.packets.append(chunks)
