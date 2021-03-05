59dc5e9d28f5cb1c0c301d2ca501d39a4a6391b0
rouyng <54250428+rouyng@users.noreply.github.com>
2021-03-05 15:53:12 -0700

    add additional metrics

--- tncexporter/metrics.py
+++ tncexporter/metrics.py
@@ -1,14 +1,34 @@
 """
-This module defines the prometheus metrics to be exported
-"""
-from prometheus_client import Counter, Histogram, Summary
+This module defines the prometheus metrics to be exported.
 
+All metrics ending with RECENT record a total over the time span set in the "summary_interval"
+parameter of tncexporter.
+"""
+from prometheus_client import Counter, Gauge, Histogram, Summary
+
+# Metrics tracking counts of frames received/decoded or transmitted
 PACKET_RX = Counter("packet_rx",
                     "Number of packets received and decoded")
 PACKET_TX = Counter("packet_tx",
                     "Number of packets transmitted")
-PACKET_DISTANCE = Histogram("packet_distance",
-                            "Distance of packets from TNC (digipeated and RF)")
-RF_PACKET_DISTANCE = Histogram("rf_packet_distance",
-                               "Distance of received packets from TNC (RF only)")
+PACKET_RX_RECENT = Gauge("packet_rx_recent",
+                         "Number of packets received over last time period")
+PACKET_TX_RECENT = Gauge("packet_tx_recent",
+                         "Number of packets transmitted over last time period")
 
+# Metrics tracking distances of received frames. Only calculated for frames that report
+# position data (APRS)
+PACKET_DISTANCE = Histogram("frame_distance",
+                            "Distance of received position packets from TNC (digipeated and RF)")
+RF_PACKET_DISTANCE = Histogram("rf_frame_distance",
+                               "Distance of received position packets from TNC (RF only)")
+MAX_DISTANCE_RECENT = Gauge("max_range_recent",
+                            "Maximum range of position frames received over last time period")
+
+# Metrics breaking down frame RX/TX activity by type or content
+FRAME_RX_UI = Counter("frame_rx_ui",
+                      "Number of monitored unproto (UI) frames received")
+FRAME_RX_S = Counter("frame_rx_s",
+                     "Number of monitored S/U frames received")
+FRAME_RX_I = Counter("frame_rx_i",
+                     "Number of monitored I frames received")
