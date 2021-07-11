915f1514cd9aea0817b63e6395020b8b11e6f255
rouyng <54250428+rouyng@users.noreply.github.com>
2021-07-11 09:33:12 -0700

    add simple functional test using subprocess, add comments to plan future test writing

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -12,12 +12,25 @@ http://appr.org.br:8901/
 from .context import tncexporter
 import pytest
 import asyncio
+import os
+import sys
+import datetime
+import pytest_mock
 
 
 # TODO: write integration tests
 # FIXME: none of this works currently
 
+# One option is to create an async AGWServer object that operates like an actual TNC
+# and run TNC exporter as an async subprocess, connecting to AGWServer over TCP.
+# A third async couroutine would then run the tests by making requests to the metrics endpoint.
+
+# Another option is to make a wrapper for the tncexporter.Exporter class that will run an event loop
+# with the addition of a tester coroutine. Calls to socket.recv() and AbstractEventLoop.sock_recv()
+# can be mocked. This is probalby the preferred approach,
+
 class AGWServer:
+    """Simulates behavior of the AGW TCP/IP interface of a TNC"""
     def __init__(self):
         # create agw server and listen for version and monitoring requests
         self.port = 8000  # port to host test server
@@ -38,41 +51,48 @@ class AGWServer:
             await asyncio.sleep(0.1)
 
 
-class TestExporterCreation:
+async def run(cmd):
+    """asynchronously run a subprocess, useful for running tncexporter in tests"""
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    start = datetime.datetime.now()
+    stdout, stderr = await proc.communicate()
 
-    @pytest.fixture
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        return stdout.decode()
+    if stderr:
+        return stderr.decode()
+
+
+def setup_env():
+    """Setup function for running subprocesses"""
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(
+            asyncio.WindowsProactorEventLoopPolicy())
+
+
+class TestExporterCreation:
+    """Test startup of TNC exporter and connection to KISS and AGW interfaces"""
+    def test_exporter_startup(self):
+        """Test that tnc exporter runs and returns the help message. The most basic function test"""
+        async def test_help_message():
+            result = await run('python -m tncexporter --help')
+            assert "Prometheus exporter for TNC metrics" in result
+            assert "--update-interval <stats data refresh interval>" in result
+        setup_env()
+        asyncio.run(test_help_message())
+
+
+class TestMetricUpdating:
     async def serve_agw_packets(self):
-        packets_1 = [
+        agw_packets_1 = [
             b'\x00\x00\x00\x00U\x00\x00\x00KF6RAL-6\x00\x00APRS\x00\x00\x00\x00\x00\x00\x8d\x00\x00\x00\x00\x00\x00\x00 1:Fm KF6RAL-6 To APRS Via SHEPRD <UI pid=F0 Len=70 PF=0 >[17:52:54]\r@112331z4044.05N/11212.65W_262/002g005t035r000p002P002h86b10160.DsVP\r\n\r\x00',
             b'\x00\x00\x00\x00U\x00\x00\x00WB7VPC\x00\x00\x00\x00T0TTXX\x00\x00\x00\x00\x7f\x00\x00\x00\x00\x00\x00\x00 1:Fm WB7VPC To T0TTXX Via KF6RAL-1,SHEPRD,WIDE2 <UI pid=F0 Len=42 F=0 >[17:53:16]\r`\'Mpl!n>/]"Cb}146.620MHz ToffON THE ROAD=\r\r\x00',
             b'\x00\x00\x00\x00U\x00\x00\x00HOLDEN\x00\x00\x00\x00APOT30\x00\x00\x00\x00l\x00\x00\x00\x00\x00\x00\x00 1:Fm HOLDEN To APOT30 Via SHEPRD,WIDE2 <UI pid=F0 Len=31 PF=0 >[17:53:57]\r!3901.82N/11209.11W# 12.6V 34F \r\x00',
             b'\x00\x00\x00\x00U\x00\x00\x00KC7YLH-2\x00\x00T0SYPT\x00\x00\x00\x00t\x00\x00\x00\x00\x00\x00\x00 1:Fm KC7YLH-2 To T0SYPT Via SHEPRD,WIDE1,WIDE2-1 <UI pid=F0 Len=29 PF=0 >[17:54:07]\r`\'Vgl Z>/\'"C9}|(2%6\'V|!wwT!|3\r\x00']
         server = AGWServer()
         await AGWServer.establish_connection()
-        await AGWServer.send_packets(packets_1)
-
-    @pytest.fixture()
-    async def only_establish_connection(self):
-        server = AGWServer()
-        await AGWServer.establish_connection()
-
-    def test_agw_connection(self, only_establish_connection):
-        """Test to see whether socket is """
-        tnc_url = "http://localhost:8000"
-        host = "127.0.0.1"
-        port = 9998
-        interval = 30
-        location = (None, None)
-        loop = asyncio.get_event_loop()
-        exp = tncexporter.TNCExporter(
-            tnc_url=tnc_url,
-            host=host,
-            port=port,
-            kiss_mode=False,
-            stats_interval=interval,
-            receiver_location=location
-        )
-        loop.run_until_complete(exp.start())
-        # FIXME: get AGWServer and exporter coroutines running in one loop so they can communicate
-        # FIXME: end metric updater task after a certain number of packets are read
-
+        await AGWServer.send_packets(agw_packets_1)
