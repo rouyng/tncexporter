c730a9689046152839681890da4389834d01bb48
rouyng <54250428+rouyng@users.noreply.github.com>
2021-07-13 08:59:51 -0700

    add test of tncexporter help argument and boilerplate to support running async subprocesses for testing

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -10,7 +10,43 @@ http://appr.org.br:8901/
 """
 
 from .context import tncexporter
+import asyncio
 import pytest
+import sys
 
 
-# TODO: integration tests
+def setup_env():
+    """Setup function for running async subprocesses"""
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(
+            asyncio.WindowsProactorEventLoopPolicy())
+
+
+async def run(cmd):
+    """asynchronously run a subprocess, useful for running tncexporter in tests"""
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await proc.communicate()
+    if stdout:
+        return stdout.decode()
+    if stderr:
+        return stderr.decode()
+
+
+class TestExporterCreation:
+    """Test startup of TNC exporter and connection to KISS and AGW interfaces"""
+
+    def test_exporter_startup(self):
+        """Test that tnc exporter runs and returns the help message. The most basic function test"""
+
+        async def test_help_message():
+            result = await run('python -m tncexporter --help')
+            assert "Prometheus exporter for TNC metrics" in result
+            assert "--update-interval <stats data refresh interval>" in result
+
+        setup_env()
+        asyncio.run(test_help_message())
+
+    # TODO: add integration tests of connection to AGW and KISS interfaces
